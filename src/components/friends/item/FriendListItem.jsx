bc33c8c808e4b8b68df00d5f44f82b95cd3e0e84
Michał Kubit <99410973+xxMANIAKxx@users.noreply.github.com>
2023-03-13 18:21:33 +0100

    poprawione

--- src/components/friends/item/FriendListItem.jsx
+++ src/components/friends/item/FriendListItem.jsx
@@ -6,7 +6,7 @@ export const FriendListItem = ({ avatar, name, isOnline }) => (
   <li className={css.item}>
     <span className={isOnline ? css.online : css.offline}></span>
     <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-        <p className={css.name}>{name}</p>
+    <p className={css.name}>{name}</p>
   </li>
 );
 
--- src/components/friends/list/FriendList.jsx
+++ src/components/friends/list/FriendList.jsx
@@ -18,7 +18,6 @@ export const FriendList = ({ friends }) => (
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
